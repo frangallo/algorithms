e4d459bc00b6b09611c5ad71906c824da03bb0da
frangallo <fgallo@me.com>
2015-10-20 16:52:29 -0700

    finished palindrome & fib

---
+++ d2.rb
@@ -0,0 +1,29 @@
+def fib(num, cache={})
+  if cache[num]
+    return cache[num]
+  elsif num == 0
+    return 0
+  elsif num == 1
+    return 1
+  else
+    value = fib(num-1, cache) + fib(num-2, cache)
+    cache[num] = value
+    return value
+  end
+end
+
+
+# function isPalindrome(string){
+#   var start = 0;
+#   var end = string.length;
+#
+#   while (start <= end){
+#     if (string[start] === string[end]){
+#       start++;
+#       end--;
+#     } else {
+#       return false
+#     }
+#   }
+#   return true;
+# }
