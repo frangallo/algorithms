d69ba8537f58362abbb7bbaf90ea8613cf30e355
frangallo <fgallo@me.com>
2015-10-21 11:45:09 -0700

    finished pair sums

--- d3.rb
+++ d3.rb
@@ -51,14 +51,13 @@ end
 def uniqe_subs(str)
   substrings = {}
   str.length.times do |start|
-    start.upto(str.length) do |end|
-      substrings[str[start..end]] = true unless substrings[str[start..end]]
+    start.upto(str.length) do |end_el|
+      substrings[str[start..end_el]] = true unless substrings[str[start..end_el]]
     end
   end
   substrings.keys
 end
 
-[1,2,3,-2,4,-5]
 
 def largest_cont_sum(array)
   sum = 0
@@ -72,4 +71,37 @@ def largest_cont_sum(array)
     end
   end
   sum
+end
+
+def silly_years(num)
+  result = []
+  year = num
+  while result.length < 10
+    result << year if silly(year)
+  end
+  result
+end
+
+def silly(num)
+  num_str = num.to_s
+  return true if num_str[0..1].to_i + num_str[2..3].to_i == num_str[1..2].to_i
+  false
+end
+
+
+def pair_sum(arr, k)
+  seen = []
+  pairs = []
+
+  arr.each do |num|
+    target = k - num
+
+    if seen.include?(target)
+      pairs.push(
+        [[num, target].min, [num, target].max]
+      )
+    end
+    seen.push(num)
+  end
+  p pairs
 end
