a850cd9de8a390611c11b1bc967fbb3b6c421c7b
frangallo <fgallo@me.com>
2015-10-22 12:19:10 -0700

    finished d6

--- d6.rb
+++ d6.rb
@@ -32,3 +32,35 @@ def is_palindrome?(string, start, len)
 
   true
 end
+
+def common_subsets(arr1,arr2)
+  subsets(intersection3(arr1,arr2))
+end
+
+def can_win(array, idx)
+  graph = Hash.new(|k,v| v = [])
+  array.each_with_index do |el, idx|
+    graph[idx] << el + idx if (el + idx).between?(0,array.length-1)
+    graph[idx] << el - idx if (el - idx).between?(0,array.length-1)
+  end
+
+  win?(graph,idx)
+end
+
+def win?(graph, idx)
+  seen = {}
+  queue = graph[idx]
+
+  until queue.empty?
+    num = queue.shift
+    next if seen[num]
+
+    seen[num] = true
+    return true if num == 0
+
+    queue.concat(graph[num])
+  end
+
+  end
+  false
+end
