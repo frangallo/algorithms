090a578d6c3bcac77185cad26655f7dab3ac5c18
frangallo <fgallo@me.com>
2015-10-23 14:35:26 -0700

    finished d7

--- d7.rb
+++ d7.rb
@@ -8,3 +8,44 @@ def weighted_random_index(array)
     return idx if value < cummulative_sum
   end
 end
+
+def move_zeros(array)
+  current_index = 0
+  num_zeros = 0
+
+  while current_index < (array.length - num_zeros)
+    current_value = array[current_index]
+
+    if current_value != 0
+      current_index += 1
+      next
+    end
+
+    back = array.length - 1 - num_zeros
+    array[current_index], array[back] =
+      array[back], array[current_index]
+    num_zeros += 1
+  end
+
+  array
+end
+
+def look_and_say(array)
+  results = []
+  count = 0
+  number = 0
+
+  array.each_with_index do |el, idx|
+    if idx == 0
+      count,number = 1,el
+    elsif el == number
+      count+= 1
+    else
+      results << [count,number]
+      count = 1
+      number = el
+    end
+  end
+  results
+
+end
