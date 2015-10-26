e08158c88ae8ce0cd020732bb8a6984e81490d2d
frangallo <fgallo@me.com>
2015-10-26 11:12:02 -0700

    finished factorials

---
+++ d10.rb
@@ -0,0 +1,29 @@
+def binary(num)
+  result  = []
+  until num == 0
+    result.unshift(num % 2)
+    num /= 2
+  end
+  result.empty? ? "0" : result.join
+end
+
+
+def factorial_it(num)
+  result = 1
+  until num == 1
+    result *= num
+    num -=1
+  end
+  result
+end
+
+def factorial_rec(num, cache)
+  if num == 0
+    cache[num] = 1
+  elsif num == 1
+    cache[num] = 1
+  else
+    val = num * factorial_rec(num-1, cache)
+    cache[num] = val
+  end
+end
