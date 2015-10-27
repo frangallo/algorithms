c04ebfa8cf3ac8cef16e2b4366089995ab054750
frangallo <fgallo@me.com>
2015-10-27 11:57:21 -0700

    finished cyclic?

--- d11.rb
+++ d11.rb
@@ -13,5 +13,30 @@ def permutations(array)
 
 end
 
+def cyclic1?(first_link)
+  set = []
 
-# [a,b]
+  current_link = first_link
+  until current_link.nil?
+    # if list is cyclic, must loop back on itself eventually
+    return true if set.include?(current_link)
+    set << current_link
+
+    current_link = current_link.next
+  end
+
+  false
+end
+
+def cyclic2?(first_link)
+  current_link1 = first_link
+  current_link2 = first_link
+  until current_link.nil?
+    current_link1 = current_link1.next
+    2.times do
+      current_link2 = current_link2.next
+    end
+    return true if current_link2 == current_link1
+  end
+  false
+end
