875b5f9bfc8ae20eb54bb8bd0ec5ac51d1c34eb7
frangallo <fgallo@me.com>
2015-11-02 08:34:04 -0800

    start heap

--- d14.rb
+++ d14.rb
@@ -81,6 +81,81 @@ class Queue
   def empty?
     @first == -1 & @last == -1
   end
+end
 
+class Stack
+  attr_accessor :top, :hold
+  def initialize
+    @top = -1
+    @hold = []
+  end
 
+  def push(val)
+    @top += 1
+    @hold[@top] = val
+  end
+
+  def pop
+    val = @hold[@top]
+    @top -=1
+    val
+  end
+
+end
+
+class Heap
+  attr_accessor :hold
+  def initialize(array)
+    array.each{|el| insert(el)}
+    @hold = []
+  end
+
+  def parent(idx)
+    [idx-1]/2
+  end
+
+  def children(idx)
+    [(idx*2)+1, (idx*2)+2].select({|el| el.between?(0,@hold.length)})
+  end
+
+  def min
+    @hold[0]
+  end
+
+  def heapify_up!
+    current = array.length-1
+    while current > 0
+      parent_idx = parent(current)
+      if @hold[current] < @hold[parent_idx]
+        swap!(current, parent_idx)
+        current = parent_idx
+      else
+        break
+      end
+    end
+  end
+
+  end
+
+  def heapify_down!
+    current = 0
+    while current < array.length-1
+      child_idx = children(current).min_by({|el| @hold[el]})
+  end
+
+  def swap!(idx1,idx2)
+    @hold[idx1], @hold[idx2] = @hold[idx2], @hold[idx1]
+  end
+
+  def insert(val)
+    @hold << val
+    heapify_up!
+    val
+  end
+
+  def pop_min
+    temp = self.min
+    heapify_down!
+    temp
+  end
 end
