0ce53e87352e994648808d1b0ed4443fa72d43bf
frangallo <fgallo@me.com>
2015-11-06 09:15:34 -0800

    cleaned up heap sort

--- d15.rb
+++ d15.rb
@@ -2,17 +2,17 @@ class Array
   def heap_sort
     return self if self.length < 2
     heapify!
+    p self
     unheapify!
     self
   end
 
 
   def heapify!
-    each_index do |idx|
-      current = idx
-      while heapify_up!(idx)
-        swap!(idx,parent(idx))
-        current = parent(idx)
+    each_index do |current|
+      while heapify_up!(current)
+        swap!(current,parent(current))
+        current = parent(current)
       end
     end
   end
@@ -20,19 +20,19 @@ class Array
   def unheapify!
     each_index do |idx|
       heap_length = length - 1 - idx
-      swap!(idx, heap_length)
-      current = idx
-      while heapify_down!(idx, heap_length)
-        child = children(idx, heap_length).min_by{|idx| self[idx]}
+      swap!(0, heap_length)
+      current = 0
+      while heapify_down!(current, heap_length)
+        child = children(current, heap_length).min_by{|idx| self[idx]}
         swap!(current, child)
         current = child
       end
     end
   end
 
-  def heapify_down!(idx)
+  def heapify_down!(idx, heap_length)
     child = children(idx, heap_length).min_by{|idx| self[idx]}
-    self[idx] > self[child]
+    self[idx] > self[child] if child
   end
 
   def heapify_up!(idx)
@@ -40,7 +40,7 @@ class Array
     self[idx] < self[parent] && idx > 0
   end
 
-  def swap(idx1, idx2)
+  def swap!(idx1, idx2)
     self[idx1], self[idx2] = self[idx2], self[idx1]
   end
 
@@ -49,6 +49,36 @@ class Array
   end
 
   def children(idx,len)
-    [(idx1*2 + 1), (idx1*2 + 2)].select{|el| el.between(0,len)}
+    [(idx*2) + 1, (idx*2) + 2].select{|el| el.between?(0,len-1)}
   end
+end
+
+
+def qs(array,start,last)
+  if start >= last
+    return array
+  else
+    pivot = random_partion(array,start,last)
+    qs(array,start,pivot-1)
+    qs(array,pivot+1,last)
+  end
+end
+
+def random_partion(array,start,last)
+  rand_idx= rand(start..last)
+  array[rand_idx], array[last] = array[last], array[rand_idx]
+  partion(array,start,last)
+end
+
+def partion(array, start, last)
+  pivot = array[last]
+  pivot_idx = start
+  start.upto(last-1) do |idx|
+    if array[idx] < pivot
+      array[idx], array[pivot_idx] = array[pivot_idx], array[idx]
+      pivot_idx +=1
+    end
+  end
+  array[-1], array[pivot_idx] = array[pivot_idx], array[-1]
+  pivot_idx
 end
